a89da58a2e45d73c1c044c237ad330df53c6a0aa
paryanimohit <mohitparyani23@gmail.com>
2021-05-08 14:02:52 -0400

    Almost Done

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,31 +1,22 @@
-// var computerScore = 0;
-// var playerScore = 0;
-
-var scores ={
-	userScore = 0,
-	computerScore = 0
-}
-
-var scores = document.getElementsByClassName("scores");
-var userResult = document.getElementsByClassName("user-result");
-var computerResult = document.getElementsByClassName("computer-result");
-var rock = document.getElementById("#rock");
-var paper = document.getElementById("#paper");
-var scissor = document.getElementById("#scissor");
+var userResult = document.getElementById("userScore");
+var computerResult = document.getElementById("computerScore");
+var rock, paper, scissor
+var userMessage = document.getElementById("usermsg");
+var computerRock, computerPaper, computerScissor;
 
 function computerPlay(){
 
     var computerSelection = Math.floor(Math.random() * 2);
-    if(computerSelection == 0){
-	showRock();
+	if(computerSelection === 0){
+		showRock();
          return "Rock";
       }  
-    if(computerSelection == 1){
-	showPaper();
+    else if(computerSelection === 1){
+		showPaper();
          return "Paper";
       }
-    if(computerSelection == 2){
-	showScissor();
+    else if(computerSelection === 2){
+		showScissor();
          return "Scissor";
       }
  }
@@ -35,118 +26,107 @@ function playRound(player){
      var computer = computerPlay();
      var playerSelection = player.toUpperCase();
      var computerSelection = computer.toUpperCase();
-     
+
 	 if(playerSelection === computerSelection){
-		
-		userResult.textContent= score.userScore;
-		computerResult.textContent = score.computerScore;
-		scores.textContent="It's a Draw! Looks like no one wants to win the Game..."
+		userMessage.textContent="It's a Draw! Looks like no one wants to win the Game..."
       }
 
      else if((playerSelection === "ROCK" && computerSelection === "SCISSOR") || (playerSelection === "PAPER" && computerSelection === "ROCK") || (playerSelection === "SCISSOR" && computerSelection === "PAPER")){
 		
-		playerScore+=1;
-        userResult.textContent=playerScore;
-		computerResult.textContent = computerScore;
-		scores.textContent = "Hurrah! You won this round";
+		userResult.textContent = Number(userResult.textContent) + 1;
+		userMessage.textContent = "Hurrah! You won this round";
 	}
      else if((playerSelection === "SCISSOR" && computerSelection === "ROCK") || (playerSelection === "ROCK" && computerSelection === "PAPER") || (playerSelection === "PAPER" && computerSelection === "SCISSOR")){
 		
-		computerScore+=1;
-		userResult.textContent=playerScore;
-		computerResult.textContent = computerScore;
-		scores.textContent="Oops! Computer won this round";
+		computerResult.textContent = Number(computerResult.textContent) + 1;
+		userMessage.textContent="Oops! Computer won this round";
 	}
      else{
-		scores.textContent="Oops that's our bad. Please reload the page";
+		 userMessage.textContent="Oops that's our bad. Please reload the page";
 	}
 }
 
-function startPlay(element1,element2,element3){
-	if(element1 === "Paper" && element2==="Scissor"){
-	
-		if(paper.style.visibility == "visible"){
+function startPlay(element1){
+	rock = document.getElementById("rock");
+	paper = document.getElementById("paper");
+	scissor = document.getElementById("scissor");
+	if(element1 === "Rock"){
 			paper.style.visibility = "hidden";
-		}
-		if(scissor.style.visibility == "visible"){
 			scissor.style.visibility = "hidden";
-		}
-	playRound(element3);
-	// showLinks();
+			playRound(element1);
 	}
-	else if(element1 === "Rock" && element2==="Scissor"){
-		if(rock.style.visibility == "visible"){
+	if(element1 === "Paper"){
 			rock.style.visibility = "hidden";	
-		}
-		
-		if(scissor.style.visibility == "visible"){
 			scissor.style.visibility = "hidden";
-		}
-	playRound(element3);
-	// showLinks();
-		
+			playRound(element1);
 	}
-	else if(element1 === "Rock" && element2==="Paper"){
-		if(rock.style.visibility == "visible"){
+	if(element1 === "Scissor"){
 			rock.style.visibility = "hidden";	
-		}
-		
-		if(paper.style.visibility == "visible"){
 			paper.style.visibility = "hidden";		
-		}
-	playRound(element3);
-	// showLinks();
+			playRound(element1);
 	}
-}
-
-function showHeading(){
-	var y=document.querySelector(".showHeading");
-	if(y.style.visibility == "hidden"){
-		y.style.visibility="visible";
-		document.querySelector(".btn").style.visibility="hidden";
-	}
-	else{
-		y.style.visibility="visible";
-	}
-}
-
-function showLinks(){
-	var x = document.querySelectorAll(".abc");
-	if(x[0].style.visibility=="hidden"){
-		x[0].style.visibility = "visible";
-		}
-	if(x[1].style.visibility=="hidden"){
-		x[1].style.visibility = "visible";
-		}
-	if(x[2].style.visibility=="hidden"){
-		x[2].style.visibility = "visible";
-		}
 }
 
 function showRock(){
-	if(rock.style.visibility == "hidden"){
-		rock.style.visibility = "visible";
-	}
+		computerPaper = document.getElementById("paperC");
+		computerScissor = document.getElementById("scissorC");
+		computerPaper.style.visibility = "hidden";
+		computerScissor.style.visibility = "hidden";
+		console.log("Rock is visible");
 }
 
 function showPaper(){
-	if(paper.style.visibility == "hidden"){
-		paper.style.visibility ="visible";
+		computerRock = document.getElementById("rockC");
+		computerScissor = document.getElementById("scissorC");
+		computerRock.style.visibility = "hidden";
+		computerScissor.style.visibility ="hidden";
+		console.log("Paper is visible");
 	}
-}
 
 function showScissor(){
-	if(scissor.style.visibility=="hidden"){
-		scissor.style.visibility = "visible";
+		computerRock = document.getElementById("rockC");
+		computerPaper = document.getElementById("paperC");
+		computerRock.style.visibility="hidden";
+		computerPaper.style.visibility = "hidden";
+		console.log("Scissor is visible");
 	}
+
+function resetScores()
+{ 
+    $( "#userScore" ).load(window.location.href + " #userScore" );
+	$( "#computerScore" ).load(window.location.href + " #computerScore" );
+	playAgain();
 }
 
-function changedClass(){
-	document.querySelector('.p').className = "r";
-	document.querySelector('.s').className = "r"; 
+function playAgain(){
+	$( "#usergame" ).load(window.location.href + " #usergame" );
+	$( "#computergame" ).load(window.location.href + " #computergame" );
+	userMessage.textContent = "Choose Wisely! Computer is Smart"
 }
 
-function unBlur(){
-	var x = document.querySelector(".game");
-	if(x.style.filter=="blur(8px)");{ x.style.filter = "blur(0px)";}
+var modal = document.getElementById("modal");
+var span = document.getElementsByClassName("close")[0];
+var userName = document.getElementById("username");
+var elname = document.getElementById("name");
+
+function updateName(){
+	userName.textContent = elname.value;
+}
+
+function openModal() {
+	modal.style.display = "block";
+  }
+  
+  span.onclick = function() {
+	modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+	if (event.target == modal) {
+	  modal.style.display = "none";
+	}
+  }
+
+function changeImage(){
+
 }
